92863c7e3762bb993a2c85c3c9efc42606c7886b
juninhokaponne <juninhokaponnemg@hotmail.com>
2020-09-20 17:12:39 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'; 
+
+
+function App = () => {
+    return (
+        <div>
+            <h1>Oi merda</h1>
+        </div>
+    );
+}
+   
+
+export default App;
